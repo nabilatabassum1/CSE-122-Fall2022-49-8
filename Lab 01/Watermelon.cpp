e8abd941b2aa96286c059b444611b2e0cbf59154
Nabila Tabassum <109521267+nabilatabassum1@users.noreply.github.com>
2022-08-06 20:25:19 +0600

    Add files via upload

---
+++ Lab 01/Watermelon.cpp
@@ -0,0 +1,19 @@
+//codeforce problem no - 4A
+//problem name - Watermelon 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int weight;
+    cin>>weight;
+    if(weight%2==0 && weight!=2 && weight>0);
+    {
+    cout<<"YES"<<endl;
+    }
+    else
+    {
+    cout<<"NO"<<endl;
+    }
+    return 0;
+}
