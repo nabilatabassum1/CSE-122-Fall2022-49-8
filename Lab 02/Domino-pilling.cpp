f8daf83c8eedd2c3a05aa4cb7b40bc21b8e2711a
Nabila Tabassum <109521267+nabilatabassum1@users.noreply.github.com>
2022-08-07 23:42:07 +0600

    Add files via upload

---
+++ Lab 02/Domino-pilling.cpp
@@ -0,0 +1,12 @@
+//Codeforce problem no - 50A
+//Problem name - Domino pilling
+#include<iostream>
+using namespace std;
+int main(){ 
+int n,m,ans=0;
+cin>>n>>m;
+ans=n*(m*n)/2;
+cout<<ans<<endl; 
+}
+return 0;
+}
